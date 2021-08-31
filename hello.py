b6d3b5cfff71dd6fb114bcb5883dc54658996836
Ishita-1999 <61704617+Ishita-1999@users.noreply.github.com>
2021-09-01 01:11:01 +0530

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,11 @@
+class Person:
+   
+    
+    def __init__(self, name):
+        self.name = name
+   
+    def func(self):
+        print('Hello, my name is', self.name)
+   
+p = Person('Ishita')
+p.func()
