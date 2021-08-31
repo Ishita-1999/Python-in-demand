b5605a4bd407f55bc233c88bde6d9737d97e60dd
Ishita-1999 <61704617+Ishita-1999@users.noreply.github.com>
2021-09-01 01:09:15 +0530

    Add files via upload

---
+++ animal.py
@@ -0,0 +1,15 @@
+class Animal:
+     
+    
+    a = "mammal"
+    b = "dog"
+ 
+    
+    def func(self):
+        print("I'm a", self.a)
+        print("I'm a", self.b)
+ 
+Rodger = Animal()
+ 
+print(Rodger.a)
+Rodger.func()
